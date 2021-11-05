b233294c471c624ee2cddb505cf6036bee701724
Nathan <nathanjsim42@gmail.com>
2021-11-05 14:38:51 +0100

    libft - corrige

---
+++ Libft/ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njennes <njennes@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 11:35:12 by njennes           #+#    #+#             */
+/*   Updated: 2021/11/05 11:39:34 by njennes          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next;
+
+	current = *lst;
+	while (current)
+	{
+		del(current->content);
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*lst = NULL;
+}
