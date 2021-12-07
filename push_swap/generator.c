3f29e80f9710326acd4e978ed4aaaaf3e51e42d7
Nathan <nathanjsim42@gmail.com>
2021-12-07 14:26:24 +0100

    wtffff je veux pleurer

--- push_swap/generator.c
+++ push_swap/generator.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int is_in(int *tab, int val)
+{
+	int i = 0;
+	while (tab[i] != 0)
+	{
+		if (tab[i] == val)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int main(int argc, char **argv)
 {
-	for (int i = 0; i < atoi(argv[1]); i++)
+	srand(time(NULL));
+	int *tab = calloc((atoi(argv[1]) + 1), sizeof(int));
+
+	int i = 0;
+	int num = 0;
+	while (i < atoi(argv[1]))
 	{
-		printf("%d ", i);
+		num = rand() % (atoi(argv[1]) * 2) + 1;
+		while (is_in(tab, num))
+			num = rand() % (atoi(argv[1]) * 2) + 1;
+		tab[i] = num;
+		i++;
 	}
+
+	for (int i = 0; tab[i]; i++)
+	{
+		printf("%d ", tab[i]);
+	}
+	
+	free(tab);
 }
