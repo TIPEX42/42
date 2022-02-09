93a9e5b40b640bd430c298d227d12113eac736de
Nathan <nathanjsim42@gmail.com>
2022-02-09 19:14:16 +0100

    auto

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && make clean"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && make fclean" \;
 
 git add .
+echo "------------------------------------------------------------"
 git status
+echo "------------------------------------------------------------"
 git commit -m "auto"
 git push
